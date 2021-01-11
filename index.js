1379d95e040323f4c7b3ed4357abdae10d82ba0a
kaushalm <kaushalmehra1987@gmail.com>
2021-01-11 10:11:05 +0530

    currying

--- index.js
+++ index.js
@@ -27,29 +27,66 @@
 
 // newfun();
 
-//POLYFILL OF BIND
+//************************POLYFILL OF BIND***********************************
 
-let name = {
-  firstname: "kaushal",
-  lastname: "mehra"
+// let name = {
+//   firstname: "kaushal",
+//   lastname: "mehra"
+// };
+
+// let printname = function(hometown, state) {
+//   console.log(
+//     this.firstname + " " + this.lastname + " " + hometown + " " + state
+//   );
+// };
+
+// let printMyName = printname.bind(name, "Delhi");
+// printMyName("Delhi");
+
+// Function.prototype.mybind = function(...args) {
+//   let obj = this;
+//   params = args.slice(1);
+//   return function(...args2) {
+//     obj.apply(args[0], [...params, ...args2]);
+//   };
+// };
+
+// let printMyName2 = printname.mybind(name, "Blr");
+// printMyName2("Kar");
+
+//*******************************CURRYING****************************************
+
+let multiply = function(x, y) {
+  console.log(x * y);
 };
 
-let printname = function(hometown, state) {
-  console.log(
-    this.firstname + " " + this.lastname + " " + hometown + " " + state
-  );
-};
+let multiplybytwo = multiply.bind(this, 2);
+multiplybytwo(3);
 
-let printMyName = printname.bind(name, "Delhi");
-printMyName("Delhi");
-
-Function.prototype.mybind = function(...args) {
-  let obj = this;
-  params = args.slice(1);
-  return function(...args2) {
-    obj.apply(args[0], [...params, ...args2]);
+let multiblycurry = function(x) {
+  return function(y) {
+    console.log(x * y);
   };
 };
 
-let printMyName2 = printname.mybind(name, "Blr");
-printMyName2("Kar");
+let mult = multiblycurry(2)(4);
+
+// let sum = function(a) {
+//   return function(b) {
+//     if (b) {
+//       return sum(a + b);
+//     } else {
+//       return a;
+//     }
+//   };
+// };
+
+// let summ = sum(1)(2)(3)(4)();
+// console.log(summ);
+
+let sum2 = a => b => (b ? sum2(a + b) : a);
+let summm = sum2(1)(2)(3)(4)(5)();
+console.log(summm);
+
+let sum = a => b => (b ? sum(a + b) : a);
+console.log(sum(10)(20)(3)(4)());
